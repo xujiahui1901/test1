c120a4bc2983cfd380f2ffdd8156f068501ae282
000 <000@192.168.127.50>
2019-05-30 15:36:44 +0800

    5

--- Test3/src/com/W.java
+++ Test3/src/com/W.java
@@ -2,6 +2,12 @@ package com;
 
 public class W {
 	public static void main(String[] args) {
-		
+		System.out.println("2");
+		System.out.println("2");
+		System.out.println("2");
+		System.out.println("2");
+		System.out.println("2");
+		System.out.println("2");
+		System.out.println("2");
 	}
 }
