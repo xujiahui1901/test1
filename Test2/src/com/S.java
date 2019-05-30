92671855f4d85467fb64b6aed8f034d34ff66020
000 <000@192.168.127.50>
2019-05-30 14:52:03 +0800

    3

--- Test2/src/com/S.java
+++ Test2/src/com/S.java
@@ -2,6 +2,10 @@ package com;
 
 public class S {
 	public static void main(String[] args) {
-		
+		System.out.println("222");
+		System.out.println("222");
+		System.out.println("222");
+		System.out.println("222");
+
 	}
 }
--- Test2/src/com/T.java
+++ Test2/src/com/T.java
@@ -3,5 +3,9 @@ package com;
 public class T {
 	public static void main(String[] args) {
 		System.out.println("1111");
+		System.out.println("1111");
+		System.out.println("1111");
+		System.out.println("1111");
+		System.out.println("1111");
 	}
 }
