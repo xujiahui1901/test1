37acae6dbf87fcdf0737e65bf6468fc261a111c0
000 <000@192.168.127.50>
2019-05-30 15:38:59 +0800

    6

--- Test3/src/com/W.java
+++ Test3/src/com/W.java
@@ -2,6 +2,10 @@ package com;
 
 public class W {
 	public static void main(String[] args) {
-		
+		System.out.println("aaa");
+		System.out.println("aaa");
+		System.out.println("aaa");
+		System.out.println("aaa");
+		System.out.println("aaa");
 	}
 }
